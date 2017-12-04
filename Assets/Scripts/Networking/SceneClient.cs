9c2bc9bb9bc6d19a25a6ffae040925b913b7a5fc
Phil Winkel <philwinkel@users.noreply.github.com>
2017-12-04 00:01:02 -0600

    initial commit

---
+++ Assets/Scripts/Networking/SceneClient.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Networking
+{
+    public class SceneClient : MonoBehaviour
+    {
+        public string SceneServerIP = "127.0.0.1";
+        public int SceneServerPort = 8888;
+        public int MinSimulatedPing = 20;
+        public int MaxSimulatedPing = 500;
+    
+        private byte _reliableChannel;
+        private byte _unreliableChannel;
+        private int _hostId = -1;
+        private int _connectionId;
+    
+        // Use this for initialization
+        void Start()
+        {
+            NetworkTransport.Init();
+
+            ConnectionConfig config = new ConnectionConfig();
+            _reliableChannel = config.AddChannel(QosType.Reliable);
+            _unreliableChannel = config.AddChannel(QosType.Unreliable);
+        
+            HostTopology topology = new HostTopology(config, 1);
+        
+#if UNITY_EDITOR
+            _hostId = NetworkTransport.AddHostWithSimulator(topology, MinSimulatedPing, MaxSimulatedPing);
+#else
+            _hostId = NetworkTransport.AddHost(topology);
+#endif
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_hostId == -1)
+                return;
+
+            int outConnectionId;
+            int outChannelId;
+            byte error;
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int receivedSize;
+            
+            NetworkEventType net = NetworkTransport.ReceiveFromHost(
+                _hostId,
+                out outConnectionId,
+                out outChannelId,
+                buffer,
+                bufferSize,
+                out receivedSize,
+                out error);
+
+            if (net == NetworkEventType.Nothing)
+                return;
+            
+            Debug.Log("SceneClient recieved NetworkEventType " + net);
+        }
+
+        public void Connect()
+        {
+            byte error;
+            int newConnectionId = NetworkTransport.Connect(_hostId, SceneServerIP, SceneServerPort, 0, out error);
+            NetworkError networkError = (NetworkError) error;
+            
+            if (newConnectionId != 0)
+            {
+                // connected
+                _connectionId = newConnectionId;
+                
+                Debug.Log("SceneClient connected. connectionId = " + newConnectionId + " error = " + networkError);
+            }
+            else
+            {
+                Debug.Log("SceneClient could not connect, error = " + networkError);
+            }
+        }
+
+        public void Disconnect()
+        {
+            byte error;
+            bool ret = NetworkTransport.Disconnect(_hostId, _connectionId, out error);
+
+            NetworkError networkError = (NetworkError) error;
+            Debug.Log("SceneClient Disconnect returned " + ret + " error " + networkError);
+        }
+
+        public void SendData()
+        {
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            byte error;
+            
+            bool result = NetworkTransport.Send(_hostId, _connectionId, _reliableChannel, buffer, bufferSize, out error);
+            NetworkError networkError = (NetworkError) error;
+            
+            Debug.Log("SceneClient SendData returned " + result + " error = " + networkError);
+        }
+    }
+}
---
+++ Assets/Scripts/Networking/SceneServer.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Networking
+{
+    public class SceneServer : MonoBehaviour
+    {
+        public int Port = 8888;
+        public int MaxConnections = 10;
+        public int MinSimulatedPing = 200;
+        public int MaxSimulatedPing = 400;
+
+        private byte _unreliableChannel;
+        private byte _reliableChannel;
+        private int _serverHostId = -1;
+
+        #region vars for update 
+
+        private int _outHostId;
+        private int _outChannelId;
+        private int _outConnectionId;
+        private byte[] _buffer;
+        private int bufferSize = 1024;
+        private int _dataLength;
+        private byte _error;
+        private Dictionary<int, IPEndPoint> _connectionDictionary 
+            = new Dictionary<int, IPEndPoint>();
+        #endregion
+
+        // Use this for initialization
+        void Start()
+        {
+            NetworkTransport.Init();
+        }
+
+        public void StartServer()
+        {
+            
+            ConnectionConfig config = new ConnectionConfig();
+            _reliableChannel = config.AddChannel(QosType.Reliable);
+            _unreliableChannel = config.AddChannel(QosType.Unreliable);
+
+            HostTopology topology = new HostTopology(config, MaxConnections);
+#if UNITY_EDITOR
+            _serverHostId = NetworkTransport.AddHostWithSimulator(topology, MinSimulatedPing, MaxSimulatedPing, Port);
+            Debug.Log("SceneServer started (editor)");
+#else
+			_serverHostId = NetworkTransport.AddHost(topology, Port);
+            Debug.Log("SceneServer started");
+#endif
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            NetworkReceive();
+        }
+
+        private void NetworkReceive()
+        {
+            if (_serverHostId == -1)
+                return;
+
+            _buffer = new byte[bufferSize];
+
+            NetworkEventType net =
+                NetworkTransport.Receive(
+                    out _outHostId,
+                    out _outConnectionId,
+                    out _outChannelId,
+                    _buffer,
+                    bufferSize,
+                    out _dataLength,
+                    out _error);
+
+            if (net == NetworkEventType.Nothing)
+                return;
+
+            NetworkError networkError = (NetworkError) _error;
+
+            Debug.Log("SceneServer received NetworkEventType: " + net + " error = " + networkError);
+
+            if (net == NetworkEventType.ConnectEvent)
+            {
+                string address;
+                int port;
+                UnityEngine.Networking.Types.NetworkID network;
+                UnityEngine.Networking.Types.NodeID dstNode;
+                NetworkTransport.GetConnectionInfo(_outHostId, _outConnectionId, out address, out port, out network,
+                    out dstNode, out _error);
+                Debug.Log("SceneServer received ConnectEvent. connectionId = " + _outConnectionId + " address = " +
+                          address);
+
+                if (!_connectionDictionary.ContainsKey(_outConnectionId))
+                {
+                    _connectionDictionary.Add(_outConnectionId, new IPEndPoint(IPAddress.Parse(address), port));
+                }
+                else
+                {
+                    Debug.Log("_connectionDictionary already ContainsKey for connectionId " + _outConnectionId);
+                }
+            }
+
+            else if (net == NetworkEventType.DisconnectEvent)
+            {
+                _connectionDictionary.Remove(_outConnectionId);
+                Debug.Log("SceneServer received DisconnectEvent. connectionId = " + _outConnectionId);
+            }
+
+            else if (net == NetworkEventType.DataEvent)
+            {
+                Debug.Log("SceneServer received DataEvent. connectionId " + _outConnectionId);
+            }
+        }
+
+        public void Stop()
+        {
+            bool ret = NetworkTransport.RemoveHost(_serverHostId);
+            if (ret)
+            {
+                _serverHostId = -1;
+            }
+            Debug.Log("SceneServer RemoveHost (stop listening) returned " + ret);
+        }
+    }
+}
